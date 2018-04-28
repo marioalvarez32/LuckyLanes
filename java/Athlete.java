6d4ccff690b9148995e6a4b6484cd54ce2b1a9b4
mcveyi17 <imcvey17@gmail.com>
2018-04-27 21:19:00 -0500

    Added Athlete class

---
+++ java/Athlete.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.java;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Ian
+ */
+public class Athlete {
+    private String name;
+    private String address;
+    private Date date;
+    private Date dateOfBirth;
+    private String phone;
+    private String school;
+    private String gender;
+    private int age;
+    private double weight;
+    
+    public Athlete(){
+        this.name = "";
+        this.address="";
+        this.date = null;
+        this.date=null;
+        this.phone="";
+        this.school="";
+        this.gender="";
+        this.age = 0;
+        this.weight=0;
+    }
+}
