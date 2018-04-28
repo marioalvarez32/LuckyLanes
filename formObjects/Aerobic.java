a4371294e5ca9c6dd95ba2e23e7a791fe064b69d
Nicholas Bentzen <bentzen.nich@gmail.com>
2018-04-27 21:09:00 -0500

    Finished Aerobics class
    Created getters and setters for v02max for various tests.

--- formObjects/Aerobic.java
+++ formObjects/Aerobic.java
@@ -6,6 +6,7 @@
 package main.formObjects;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+
 /**
  *
  * @author nicholas bentzen
@@ -13,29 +14,83 @@ import java.util.ArrayList;
  * pdf document.
  * 
  */
-
 public class Aerobic {
     private int sexInt, age;
-    private String sex, rating;
-    private int[] maleRange = new int[5];
-    private int[] femaleRange = new int[5];
+    private double v02bench, v02walk, v02Rockport;
+    private String sex, benchRating;
    
     //constructor takes an integer age, and string sex.
-    public Aerobic(int age, int heartRate, String sex) throws FileNotFoundException{
+    public Aerobic(int age, String sex) throws FileNotFoundException{
+        this.age = age;
         if(sex == "male") this.sexInt = 1;
         else this.sexInt = 0;
         //this.age = age;
         this.sex = sex;
-        int[] ageRange = {18, 26, 36, 46, 56, 65};
-        int ageGroup = this.calcIndex(age, ageRange);
-        HeartBeatFileReader reader = new HeartBeatFileReader("womenHB.txt");
-        int[] heartArr = reader.getArr(ageGroup);
-        
-        
-        
-        
-        
     }
+    
+    //sets the v02max for the benchtest.
+    public void setBench(int heartRate){
+        this.v02bench = (-0.9675 * heartRate) + 76.710;
+    }
+    
+    //returns the v02max for the benchtest.
+    public double getBench(){
+        return this.v02bench;
+    }
+    
+    //implements bench test
+    public void setBenchRating(int heartRate, String fileName) throws FileNotFoundException{
+        int[] ageRange = {18, 26, 36, 46, 56, 65};
+        int ageGroup = this.calcIndex(this.age, ageRange);
+        String[] rating = {
+            "Excellent", "Excellent/Good", "Good", "Good/Above Average", "Above Average",
+            "Above Average/Average", "Average", "Average/Below Average", "Poor/Very Poor", "Poor"
+        };
+        HeartBeatFileReader reader = new HeartBeatFileReader(fileName);
+        int[] heartArr = reader.getArr(ageGroup);
+        int heartGroup = this.calcIndex(heartRate, heartArr);
+        this.benchRating = rating[heartGroup];
+    }
+    
+    //returns the rating, Ex: Excellent, Good, etc.
+    public String getBenchRating(){
+        return this.benchRating;
+    }
+    
+    //sets the v02max for the RockPort test
+    public void setRockport(double weight, double walkTime, int heartRate){
+        double a, b, c, d, e;
+        if(this.age <= 25){
+            a = 88.767;
+            b = 8.892;
+            c = 0.2109;
+            d = 1.4537;
+            e = 0.1194;
+        }else{
+            a = 88.767;
+            b = 6.315;
+            c = 0.3877;
+            d = 0.1692;
+            e = 3.2649;
+        }
+            this.v02Rockport = a + (this.sexInt * 8.892) - (weight * 0.2109) - (walkTime * 1.4537) - (heartRate * 0.1194);
+    }
+    
+    
+    //returns the v02max for the rockport test
+    public double getRockport(){
+        return this.v02Rockport;
+    }
+    
+    //calculates 12 min walk in meters
+    public void set12minWalk(double distance){
+        this.v02walk = (distance - 504.9)/44.73;
+    }
+    
+   //returns the v02max for the 12 min walk test
+   public double get12minWalk(){
+       return this.v02walk;
+   }
     
     //helper method: calculates the age demographic of an athlete to determine
     //appropriate range of heart beats for a given score.
@@ -54,19 +109,6 @@ public class Aerobic {
         return -1;
     }
     
-    // helper method: determines score of Excellent, Above Average, etc. by looking
-    // at the age range,gender, and heart rate.
-    private void calcRating(){
-        if(this.sex == "male"){
-            
-        }else{
-            
-        }
-        //if sex = male use mens rating table, else use female ratings table
-        //need to calculate 3 scores: Bench, 12 min walk, and Rockport walk
-        // need to calculte VO2 max for each score
-    }
-    
     //method takes in 3 numbers: mid, low  high; method returns
     // if mid is >= low and < high.
     private boolean compareNums(int mid, int low, int high){
@@ -74,13 +116,5 @@ public class Aerobic {
     }
     
     
-    //returns the rating, Ex: Excellent, Good, etc.
-    public String getRating(){
-        return this.rating;
-    }
     
-    //returns the number that determines the rating
-    public int getScore(){
-        return 0;
-    }
 }
